eeea636c1c0f89ae534b01d04f1d88bc00330408
oZeca <ze.lupicaetano@gmail.com>
2024-05-16 14:47:58 +0100

    container gut update

--- src/app/components/container.tsx
+++ src/app/components/container.tsx
@@ -8,7 +8,7 @@ type props = {
 export default function Container({children ,className}: props) {
   return (
     <div className={clsx(
-      `flex flex-col md:flex-row w-full mx-auto px-4 gap-20 sm:max-w-[640px] md:max-w-[768px] lg:max-w-[1024px] xl:max-w-[1357px]`, 
+      `flex flex-col md:flex-row w-full mx-auto px-6 gap-20 sm:max-w-[640px] md:max-w-[768px] lg:max-w-[1024px] xl:max-w-[1357px]`, 
       className)
     }>
       {children}
